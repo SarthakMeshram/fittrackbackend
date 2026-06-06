8ed02ee0ff6050ef66f31a682ffa5ed8f529d9a2
Sarthak Meshram <sarthakm@Sarthaks-MacBook-Air.local>
2026-06-06 22:16:17 +0530

    Initial backend setup

---
+++ src/main/java/com/sarthak/fittrackbackend/entity/User.java
@@ -0,0 +1,35 @@
+package com.sarthak.fittrackbackend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
